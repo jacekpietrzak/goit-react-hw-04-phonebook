3b864103ee4bcaaf0d9a44602bcbf141d4426aa9
Jacek Pietrzak <jacekpietrzak@me.com>
2023-01-27 18:16:06 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,15 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
-const DATA = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 const App = () => {
-  const [contacts, setContacts] = useState(DATA);
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts'))
+  );
   const [filter, setFilter] = useState('');
 
   const handleFilter = event => {
     setFilter(event.target.value.toLowerCase());
   };
-
-  const loadLocalStorage = () => {
-    const localStorageContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (localStorageContacts) {
-      setContacts(localStorageContacts);
-    }
-  };
-
-  loadLocalStorage;
 
   const addContact = contact => {
     const newContact = {
